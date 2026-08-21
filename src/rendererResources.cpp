5ebd7f390dc082382150154e7fa372af20569194
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2026-08-21 01:03:53 +0100

    added instance of VulkanGraphicsPipeline in RenderingResources

--- src/rendererResources.cpp
+++ src/rendererResources.cpp
@@ -57,5 +57,35 @@ namespace projectv
         {
             return m_pipelineLayout;
         }
+
+        rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline()
+        {
+            return m_graphicsPipeline;
+        }
+
+        const rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline() const
+        {
+            return m_graphicsPipeline;
+        }
+
+        rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout()
+        {
+            return m_pipelineLayout;
+        }
+
+        const rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout() const
+        {
+            return m_pipelineLayout;
+        }
+
+        rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline()
+        {
+            return m_graphicsPipeline;
+        }
+
+        const rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline() const
+        {
+            return m_graphicsPipeline;
+        }
     }
 }
