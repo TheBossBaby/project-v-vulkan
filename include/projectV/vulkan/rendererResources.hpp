e3a057ace91ea9090ac23100b919bc4ed4b8b692
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2026-08-21 01:17:37 +0100

    fix: added missing include

--- include/projectV/vulkan/rendererResources.hpp
+++ include/projectV/vulkan/rendererResources.hpp
@@ -4,7 +4,6 @@
 #include <vulkan/rendererResources/vulkanFramebuffer.hpp>
 #include <vulkan/rendererResources/vulkanPipelineLayout.hpp>
 #include <vulkan/rendererResources/vulkanGraphicsPipeline.hpp>
-#include <vulkan/rendererResources/vulkanPipelineLayout.hpp>
 
 #include <vector>
 namespace projectv
