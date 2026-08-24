8914e32aa0728a1c8a4fa6720c3cd5c23e976bc4
Pranav Kushwaha <30798744+TheBossBaby@users.noreply.github.com>
2026-08-25 00:19:13 +0100

    merge-fix: Removed repeated include and unwanted methods

--- src/rendererResources.cpp
+++ src/rendererResources.cpp
@@ -47,45 +47,5 @@ namespace projectv
 
             return &m_graphicsPipelines[handle.index];
         }
-
-        rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout()
-        {
-            return m_pipelineLayout;
-        }
-
-        const rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout() const
-        {
-            return m_pipelineLayout;
-        }
-
-        rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline()
-        {
-            return m_graphicsPipeline;
-        }
-
-        const rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline() const
-        {
-            return m_graphicsPipeline;
-        }
-
-        rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout()
-        {
-            return m_pipelineLayout;
-        }
-
-        const rendererResources::VulkanPipelineLayout &RendererResources::pipelineLayout() const
-        {
-            return m_pipelineLayout;
-        }
-
-        rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline()
-        {
-            return m_graphicsPipeline;
-        }
-
-        const rendererResources::VulkanGraphicsPipeline &RendererResources::graphicsPipeline() const
-        {
-            return m_graphicsPipeline;
-        }
     }
 }
